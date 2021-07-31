ec8a28538783f756d5009e17111b30ac3168ef64
Ecthys <81968564+echthys@users.noreply.github.com>
2021-07-31 13:18:33 +0100

    feat; improved error handling

--- src/client/js/holidayData.js
+++ src/client/js/holidayData.js
@@ -2,8 +2,14 @@ function getHolidayData() {
     const city = document.getElementById("city").value
     const country = document.getElementById("country").value
     const date = document.getElementById("date").value
+    if (city && country && date) {
+        return { city: city, country: country, date: date }
+    }
+    else {
+        alert("You have entered a null value please try again.")
+        return false
+    }
 
-    return { city: city, country: country, date: date }
 }
 
 export { getHolidayData }
